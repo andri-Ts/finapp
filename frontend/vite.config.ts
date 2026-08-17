27c0960c4409f89a8021b104792e9f9e43bb2894
Po Bidau <pobidau@protonmail.com>
2026-08-17 10:31:20 +0200

    chore: ajout icon, ignore local mardown file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,37 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'FinApp',
+        short_name: 'FinApp',
+        description: 'Gestion simple et intuitive de votre budget',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          {
+            src: '/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 });
