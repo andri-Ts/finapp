dd70b17ab4d6df41f06a0dacd6c1f10760f3b2a3
Po Bidau <pobidau@protonmail.com>
2026-08-21 23:55:53 +0200

    feat: migration vers tanstack query

--- frontend/src/pages/account/AccountsPage.tsx
+++ frontend/src/pages/account/AccountsPage.tsx
@@ -1,7 +1,7 @@
 import PageHeader from '@/components/layout/pageHeader';
 import styles from './accountsPage.module.css';
 import AccountList from '@/features/accounts/components/accountList';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import type { IAccount } from '@/types/account.types';
 import {
   archiveAccount,
@@ -11,65 +11,97 @@ import {
 import Button from '@/components/ui/Button';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'sonner';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { queryClient } from '@/lib/queryClient';
 
 function AccountsPage() {
-  const [accounts, setAccounts] = useState<IAccount[]>([]);
-  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    async function loadAccount() {
-      try {
-        const data = await getAllAccount();
-        setAccounts(data.accounts);
-      } catch (error) {
-        console.error('Erreur lors du chargement des comptes: ', error);
-      } finally {
-        setLoading(false);
-      }
-    }
+  // =========================================================
+  // Utiliasation de useQuery
+  // =========================================================
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['accounts'], // clé qui idientifi la cache contenant la liste des compte
+    queryFn: getAllAccount,
+  });
 
-    loadAccount();
-  }, []);
+  const accounts: IAccount[] = data?.accounts ?? []; // si data n'est pas dispon, on revoie tab vide
 
-  const handleArchiveAccount = async (id: string) => {
-    try {
-      const message = await archiveAccount(id);
+  // mutation pour archiver un compte
+  const archiveMutation = useMutation({
+    mutationFn: archiveAccount,
 
-      setAccounts((currentAccounts) =>
-        currentAccounts.filter((account) => account.id !== id),
-      );
-
+    onSuccess: (message) => {
+      queryClient.invalidateQueries({ queryKey: ['accounts'] }); // une fois archiver, on invalide la liste des comptes (du cache)
       toast.success(message);
-    } catch (error) {
+    },
+
+    onError: (error) => {
       console.error("Erreur lors de l'archivage :", error);
       toast.error("Impossible d'archiver le compte");
-    }
-  };
+    },
+  });
 
-  const handleSetDefault = async (id: string) => {
-    try {
-      await setDefaultAccount(id);
+  // mutation pour définir compte par défaut
+  const defaultMutation = useMutation({
+    mutationFn: setDefaultAccount,
 
-      setAccounts((currentAccounts) =>
-        currentAccounts.map((account) => ({
-          ...account,
-          isDefault: account.id === id,
-        })),
-      );
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['accounts'],
+      });
+      toast.success('Compte par défaut moidifié');
+    },
 
-      toast.success('Compte par défaut modifié');
-    } catch (error) {
+    onError: (error) => {
       console.error(
         'Erreur lors de la modification du compte par défaut : ',
         error,
       );
+
       toast.error('Impossible de modifier le compte par défaut');
-    }
+    },
+  });
+
+  // useEffect(() => {
+  //   async function loadAccount() {
+  //     try {
+  //       const data = await getAllAccount();
+  //       setAccounts(data.accounts);
+  //     } catch (error) {
+  //       console.error('Erreur lors du chargement des comptes: ', error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   }
+
+  //   loadAccount();
+  // }, []);
+
+  // =========================================================
+  // Handlers
+  // =========================================================
+
+  const handleArchiveAccount = async (id: string) => {
+    // On ne modifie plus directement le tableau accounts.
+    // On demande à la mutation d'appeler l'API.
+    archiveMutation.mutate(id);
   };
 
-  if (loading) {
+  const handleSetDefault = async (id: string) => {
+    defaultMutation.mutate(id);
+  };
+
+  // =========================================================
+  // Gestion du chargement, erreur
+  // =========================================================
+
+  if (isLoading) {
     return <p>Chargement...</p>;
+  }
+
+  if (isError) {
+    return <p>Impossible de charger les comptes.</p>;
   }
 
   return (
--- frontend/src/pages/category/CategoryPage.tsx
+++ frontend/src/pages/category/CategoryPage.tsx
@@ -2,7 +2,7 @@ import PageHeader from '@/components/layout/pageHeader';
 import styles from './categoryPage.module.css';
 import CategoryList from '@/features/categories/components/categoryList';
 // import { mockCategories } from '@/mocks/categories.mock';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import type { ICategory } from '@/types/category.types';
 import {
   archiveCategory,
@@ -10,49 +10,71 @@ import {
 } from '@/features/categories/api/categoryApi';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'sonner';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { queryClient } from '@/lib/queryClient';
 
 function CategoryPage() {
-  const [categories, setCategories] = useState<ICategory[]>([]);
-  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    async function loadingCategories() {
-      try {
-        const data = await getAllCategory();
-        console.log('categories: ', data);
-        setCategories(data.categories);
-      } catch (error) {
-        console.error('Erreur lors du chargement des catégories: ', error);
-      } finally {
-        setLoading(false);
-      }
-    }
+  // =========================================================
+  // Utiliasation de useQuery
+  // =========================================================
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['categories'],
+    queryFn: getAllCategory,
+  });
 
-    loadingCategories();
-  }, []);
-  console.log('categories: ', categories);
+  const categories: ICategory[] = data?.categories ?? [];
+
+  const archiveMutation = useMutation({
+    mutationFn: archiveCategory,
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['categories'],
+      });
+      toast.success('Catégories archivées avec succes');
+    },
+
+    onError: (error) => {
+      console.error("Erreur lors de l'archivage de la catégorie :", error);
+      toast.error("Impossible d'archiver la catégorie");
+    },
+  });
+
+  // useEffect(() => {
+  //   async function loadingCategories() {
+  //     try {
+  //       const data = await getAllCategory();
+  //       console.log('categories: ', data);
+  //       setCategories(data.categories);
+  //     } catch (error) {
+  //       console.error('Erreur lors du chargement des catégories: ', error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   }
+
+  //   loadingCategories();
+  // }, []);
+  // console.log('categories: ', categories);
 
   const handleEditCategory = (category: ICategory) => {
     navigate(`/categories/${category.id}/edit`);
   };
 
   const handleArchiveCategory = async (id: string) => {
-    try {
-      await archiveCategory(id);
-
-      setCategories((prevValue) =>
-        prevValue.filter((category) => category.id !== id),
-      );
-      toast.success('Catégorie archivée avec succès');
-    } catch (error) {
-      console.error("Erreurs lors de l'archivage de la catégorie: ", error);
-      toast.error("Impossible d'archiver la catégorie");
-    }
+    // On ne modifie plus directement categories avec setState.
+    // La mutation appelle l'API puis invalide le cache.
+    archiveMutation.mutate(id);
   };
 
-  if (loading) {
+  if (isLoading) {
     return <p>Chargement...</p>;
+  }
+
+  if (isError) {
+    return <p>Impossible de charger les catégories.</p>;
   }
 
   return (
